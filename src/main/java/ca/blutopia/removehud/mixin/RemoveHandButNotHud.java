f81e76478aa0023164806c0b21b1991f4564f4d1
Israel Aristide <44146685+IsraelAristide@users.noreply.github.com>
2022-02-22 11:13:23 -0500

    Update version number

--- src/main/java/ca/blutopia/removehud/mixin/RemoveHandButNotHud.java
+++ src/main/java/ca/blutopia/removehud/mixin/RemoveHandButNotHud.java
@@ -15,7 +15,6 @@ public abstract class RemoveHandButNotHud {
         if (ModConfig.INSTANCE.removeHand) {
             info.cancel();
         }
-
     }
 
 }
--- src/main/java/ca/blutopia/removehud/mixin/RemoveHudButNotHand.java
+++ src/main/java/ca/blutopia/removehud/mixin/RemoveHudButNotHand.java
@@ -192,5 +192,4 @@ public abstract class RemoveHudButNotHand {
         }
     }
 
-
 }
